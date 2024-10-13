9895689596b494e949ad52fddd0400ecef6260c1
Strawcabbage <Jackson@hulitech.com>
2024-10-13 18:23:02 -0500

    Fixed another bug where I was freeing the memory of linecopy to early. Added comments for the whole project

--- src/c_shell.c
+++ src/c_shell.c
@@ -7,29 +7,33 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-
+//Function declerations
 char *csh_read_line(void);
-char **csh_parse_line(char*);
+char **csh_parse_line(char*, char*);
 void csh_loop();
 int csh_execute(char**);
 int csh_launch(char**);
 int csh_exit();
 int csh_echo();
 
+//Global variables
 const char *built_in_strs[] = {"exit", "echo"};
 char *line;
 
+//Function pointer to built in commands (exit, echo)
 int (*built_in_func[]) (char**) = {
     &csh_exit,
     &csh_echo
 };
 
+//Built in function exit. This function exits the shell
 int csh_exit() {
 
     return 0;
     
 }
 
+//Built in function echo. This function prints whatever is entered after echo to the terminal
 int csh_echo() {
     
     char *first_token;
@@ -48,40 +52,54 @@ int csh_echo() {
 
 }
 
+//Main function which starts the shell input loop function
 int main(int argc, char **argv) {
     
     csh_loop();
     return 0;
 }
 
+//The shell loop function which loops through recieving and reading input until there is a request to exit
 void csh_loop(void) {
     
     char **args;
     int status;
+    char *linecopy;
 
+    //While loop to read read, parse, and execute input
     do {
-    
+       
        printf("> ");
 
        line = csh_read_line();
-       args = csh_parse_line(line);
+       linecopy = malloc(strlen(line) + 1);
+       args = csh_parse_line(line, linecopy);
        status = csh_execute(args);
-        
+       
+       //Freeing memory allocated to args and line to prevent memory leaks before looping again or exiting
        free(line);
        free(args);
+       free(linecopy);
 
     } while (status);
     
 }
 
+//Function to read input
 char *csh_read_line(void) {
-
+    
+    //Declaring variables
     size_t bufsize = 0;
     char *buffer = NULL;    
     
-
+    /*
+     * Using getline() to dynamically allocate memory
+     * to the char pointer, buffer, then checking if
+     * the function was successful by equating it to -1
+     * (the output if EOF is reached or if there's an error)
+    */
     if (getline(&buffer, &bufsize, stdin) == -1) {
-
+        //If EOF is reached the the function was a success, else it failed
         if (feof(stdin)) {
             exit(EXIT_SUCCESS);
         } else {
@@ -89,47 +107,59 @@ char *csh_read_line(void) {
             exit(EXIT_FAILURE);
         }
     }
-
+    //Return buffer after getline() was used
     return buffer;
 
 }
 
-char **csh_parse_line(char *line) {
-
+//Parsing the read input into a char pointer to an array of Strings
+char **csh_parse_line(char *line, char *linecopy) {
+    
+    //Declaring Variables
     char *str;
     size_t position = 0;
     size_t bufsize = 64;
     char **strs;
     char *delims = DELIMS;
-
-    char *linecopy = malloc(strlen(line) + 1);
-
+   
+   
+    //Making sure dynamic memory allocation succeeded
     if (linecopy == NULL) {
         perror("Error allocating memory");
         free(linecopy);
         exit(EXIT_FAILURE);
     }
-
+    
+    //Deep copying the char pointer line so it can be used later in the echo built in function
     strcpy(linecopy, line);
-
+    
+    //dyamically allocating memory to strs to allow for 64 char pointers
     strs = malloc(bufsize * sizeof(char*));
- 
+    
+    //Making sure dynamic memory allocation succeeded
     if (strs == NULL) {
         perror("Error allocating memory");
         free(strs);
         exit(EXIT_FAILURE);
     }
     
+    //Initializing strtok with string linecopy and delimeters then assiging the first token to str
     str = strtok(linecopy, delims);
-
+    
+    //This while loop will parse all of line copy and add each token to strs
     while (str != NULL) {
-         
+        
+        //Adding the first token to strs using position as a guide
         strs[position] = str;
-
+        
+        //Checking if the memory for string needs to be expanded by seeing if position is greater than the bufsize allocated
         if (position >= bufsize) {
             bufsize *= 2;
-            strs = realloc(strs, bufsize * sizeof(char*));
 
+            //Using realloc for new allocation
+            strs = realloc(strs, bufsize * sizeof(char*));
+            
+            //Making sure dynamic memory allocation succeeded
             if (strs == NULL) {
                perror("Error allocating memory");
                free(strs);
@@ -137,22 +167,32 @@ char **csh_parse_line(char *line) {
             }
         }
         
+        //Null terminating str before adding it to str again
         str = strtok(NULL, delims);
         position++;
 
     }
     
+    //Null ending strs then returning it
     strs[position] = NULL;
     return strs;
 }
 
+//Function for executing a program on a new process
 int csh_launch(char **args) {
     
+    //Declaring variables
     pid_t pid, wpid;
     int status;
-
+    
+    //Forking the parent proccess into a child and the parent
     pid = fork();
-        
+    
+    /*
+     * Checking if fork failed then executing new program on child
+     * process using execvp(). Also telling the parent process to
+     * wait for child process to signal that it is finished.
+    */
     if (pid<0) {
         perror("Fork fail");
         exit(EXIT_FAILURE);
@@ -171,20 +211,26 @@ int csh_launch(char **args) {
     return 1;
 }
 
+/*
+ * Function for deciding whether a command is for a built in
+ * function or a system command by using built_in_strs
+ * then returning a status after checking
+*/
 int csh_execute(char **args) {
     
+    //If the argument entered is empty or failed, then the loop is restarted and input is recieved again
     if (args[0] == NULL) {
         return 0;
     }
-
+    
+    //Checkin whether the command is built in by looping through built_in_strs
     for (int i = 0; i < (sizeof(built_in_strs) / sizeof(built_in_strs[0])); i++) {
         if (strcmp(args[0], built_in_strs[i]) == 0) {
-
+            //The command has evaluated to be a built in command
             return (*built_in_func[i])(args);
         }
     }
+    //The command is not built in and now we run the function to launch the new command
     return csh_launch(args);
 }
-
-
 
