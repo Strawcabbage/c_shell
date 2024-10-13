2851a636adf16a48399c5bd983ea7d61a944a4ed
Strawcabbage <Jackson@hulitech.com>
2024-10-13 17:13:57 -0500

    Fixed bug where a null pointer was attempting to be accessed, also added the built in functions cd and echo

--- src/c_shell.c
+++ src/c_shell.c
@@ -1,4 +1,5 @@
 #define _GNU_SOURCE
+#define DELIMS " /\r\a\t\n" 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,8 +11,42 @@
 char *csh_read_line(void);
 char **csh_parse_line(char*);
 void csh_loop();
-int csh_exec(char**);
+int csh_execute(char**);
 int csh_launch(char**);
+int csh_exit();
+int csh_echo();
+
+const char *built_in_strs[] = {"exit", "echo"};
+char *line;
+
+int (*built_in_func[]) (char**) = {
+    &csh_exit,
+    &csh_echo
+};
+
+int csh_exit() {
+
+    return 0;
+    
+}
+
+int csh_echo() {
+    
+    char *first_token;
+    char *rest_of_line;
+    char *delim = DELIMS;
+
+    first_token = strtok(line, delim);
+
+    rest_of_line = strtok(NULL, "");
+
+    if (rest_of_line != NULL) {
+        printf("%s", rest_of_line);
+    }
+
+    return 1;
+
+}
 
 int main(int argc, char **argv) {
     
@@ -21,7 +56,6 @@ int main(int argc, char **argv) {
 
 void csh_loop(void) {
     
-    char *line;
     char **args;
     int status;
 
@@ -31,7 +65,7 @@ void csh_loop(void) {
 
        line = csh_read_line();
        args = csh_parse_line(line);
-       status = csh_exec(args);
+       status = csh_execute(args);
         
        free(line);
        free(args);
@@ -43,9 +77,11 @@ void csh_loop(void) {
 char *csh_read_line(void) {
 
     size_t bufsize = 0;
-    char *buffer = NULL;
+    char *buffer = NULL;    
+    
 
     if (getline(&buffer, &bufsize, stdin) == -1) {
+
         if (feof(stdin)) {
             exit(EXIT_SUCCESS);
         } else {
@@ -53,30 +89,41 @@ char *csh_read_line(void) {
             exit(EXIT_FAILURE);
         }
     }
-    
+
     return buffer;
 
 }
 
 char **csh_parse_line(char *line) {
-    
+
     char *str;
     size_t position = 0;
     size_t bufsize = 64;
-    char **strs = malloc(bufsize * sizeof(char*));
-    char *delims = " /\r\a\t\n";
-    
+    char **strs;
+    char *delims = DELIMS;
 
+    char *linecopy = malloc(strlen(line) + 1);
+
+    if (linecopy == NULL) {
+        perror("Error allocating memory");
+        free(linecopy);
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(linecopy, line);
+
+    strs = malloc(bufsize * sizeof(char*));
+ 
     if (strs == NULL) {
         perror("Error allocating memory");
         free(strs);
         exit(EXIT_FAILURE);
     }
     
-    str = strtok(line, delims);
+    str = strtok(linecopy, delims);
 
     while (str != NULL) {
-        
+         
         strs[position] = str;
 
         if (position >= bufsize) {
@@ -99,45 +146,45 @@ char **csh_parse_line(char *line) {
     return strs;
 }
 
-int csh_exec(char **args) {
-    
-    const char *built_in[] = {"cd", "echo"};
-    int built_in_exe = 0;
-
-    for (int i = 0; i < (sizeof(built_in) / sizeof(built_in[0])); i++) {
-        if (strcmp(args[0], built_in[i]) == 0) {
-            built_in_exe = 1;
-        }
-    }
-
-    if (built_in_exe) {
-        return csh_launch(args);
-        
-    } else {
-        pid_t pid = fork();
-        
-        if (pid<0) {
-            perror("Fork fail");
-            exit(EXIT_FAILURE);
-            return 0;
-        }
-        if (!pid) {
-            int status_code = execvp(args[0], args);
-            if (status_code == -1) {
-                perror("Process did not execute");
-                exit(EXIT_FAILURE);
-                return 0;
-            }
-            return 1;
-        } else {
-            exit(EXIT_SUCCESS);
-            return 1;
-        }
-    }
-
-}
-
 int csh_launch(char **args) {
-    printf("You're not supposed to be here");
-    return 0;
+    
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+        
+    if (pid<0) {
+        perror("Fork fail");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        int status_code = execvp(args[0], args);
+        if (status_code == -1) {
+            perror("Process did not execute");
+        }
+        exit(EXIT_FAILURE);
+    } else {
+          
+        do { 
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
 }
+
+int csh_execute(char **args) {
+    
+    if (args[0] == NULL) {
+        return 0;
+    }
+
+    for (int i = 0; i < (sizeof(built_in_strs) / sizeof(built_in_strs[0])); i++) {
+        if (strcmp(args[0], built_in_strs[i]) == 0) {
+
+            return (*built_in_func[i])(args);
+        }
+    }
+    return csh_launch(args);
+}
+
+
+
